a53c7a4ff15e75af9099eb18092a5fd7e9b565a8
Yantogondrong <44944945+Yantogondrong@users.noreply.github.com>
2018-11-13 18:17:43 +0700

    Add files via upload

---
+++ Index.php
@@ -0,0 +1,378 @@
+<!DOCTYPE html>
+<html>
+<head>
+<script type="text/javascript">
+alert('touched by WILL')
+</script>
+	<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Hacked VV.H.0</title>
+	<link REL="SHORTCUT ICON" HREF="http://i45.tinypic.com/1o6cqx.png">
+	<link href='https://fonts.googleapis.com/css?family=Orbitron' rel='stylesheet' type='text/css'>
+
+	<style>
+		* {
+			cursor: url(http://cur.cursors-4u.net/toons/too-12/too1105.cur), auto !important;
+		}
+		body{
+			 background	: black;
+	         /*background-image: url(http://bestanimations.com/Computers/funny-computer-matrix-animated-gif-42.gif);*/
+	         background-repeat:no-repeat;
+	         background-attachment: fixed;
+	         background-size: 1350px;
+	         font-family	: Courier new;
+	         text-align	: center;
+	         color		: #fff;
+	        margin:0;
+			padding:0;
+			height:100%;
+		  	-webkit-animation: noise-3 1s linear infinite;
+		    animation: noise-3 1s linear infinite;
+		  	overflow: default;
+		}
+		.wrapper{
+			height: 100vh;
+		}
+		pre {
+         	margin-top: 9%;
+        }
+        a {
+
+			text-decoration:none;
+			color:
+			#ff0000;
+		}
+		.dud {
+		  color: red;
+		}
+
+		.text {
+		  font-weight: bold;
+		  font-size: 250px;
+		  color: #fafafa;
+		  font-family: 'Roboto Mono', monospace;
+		    height: 85vh;
+    		padding-top: 15vh;
+		}
+		.info {
+		    text-align: center;
+		    width: 200px;
+		    height: 60px;
+		    position: absolute;
+		    top: 70vh;
+		    bottom: 0;
+		    left: 20px;
+		    right: 0;
+		    -webkit-animation: noise-3 1s linear infinite;
+		    animation: noise-3 1s linear infinite;
+		}
+
+
+		.info:after {
+			content: 'OWNED';
+			font-family: OCR-A;
+			font-size: 100px;
+			text-align: center;
+			width: 800px;
+			margin: 70vh;
+			position: absolute;
+			top: 70vh;
+			bottom: 0;
+			left: 40px;
+			right: 0;
+			opacity: 0;
+			color: white;
+			-webkit-animation: noise-1 .2s linear infinite;
+		    animation: noise-1 .2s linear infinite;
+		}
+
+		@-webkit-keyframes noise-1 {
+			0%, 20%, 40%, 60%, 70%, 90% {opacity: 0;}
+			10% {opacity: .1;}
+			50% {opacity: .5; left: -6px;}
+			80% {opacity: .3;}
+			100% {opacity: .6; left: 2px;}
+		}
+
+		@keyframes noise-1 {
+			0%, 20%, 40%, 60%, 70%, 90% {opacity: 0;}
+			10% {opacity: .1;}
+			50% {opacity: .5; left: -6px;}
+			80% {opacity: .3;}
+			100% {opacity: .6; left: 2px;}
+		}
+
+		@-webkit-keyframes noise-2 {
+			0%, 20%, 40%, 60%, 70%, 90% {opacity: 0;}
+			10% {opacity: .1;}
+			50% {opacity: .5; left: 6px;}
+			80% {opacity: .3;}
+			100% {opacity: .6; left: -2px;}
+		}
+
+		@keyframes noise-2 {
+			0%, 20%, 40%, 60%, 70%, 90% {opacity: 0;}
+			10% {opacity: .1;}
+			50% {opacity: .5; left: 6px;}
+			80% {opacity: .3;}
+			100% {opacity: .6; left: -2px;}
+		}
+
+		@-webkit-keyframes noise {
+			0%, 3%, 5%, 42%, 44%, 100% {opacity: 1; -webkit-transform: scaleY(1); transform: scaleY(1);}  
+			4.3% {opacity: 1; -webkit-transform: scaleY(1.7); transform: scaleY(1.7);}
+			43% {opacity: 1; -webkit-transform: scaleX(1.5); transform: scaleX(1.5);}
+		}
+
+		@keyframes noise {
+			0%, 3%, 5%, 42%, 44%, 100% {opacity: 1; -webkit-transform: scaleY(1); transform: scaleY(1);}  
+			4.3% {opacity: 1; -webkit-transform: scaleY(1.7); transform: scaleY(1.7);}
+			43% {opacity: 1; -webkit-transform: scaleX(1.5); transform: scaleX(1.5);}
+		}
+
+		@-webkit-keyframes noise-3 {
+			0%,3%,5%,42%,44%,100% {opacity: 1; -webkit-transform: scaleY(1); transform: scaleY(1);}
+			4.3% {opacity: 1; -webkit-transform: scaleY(4); transform: scaleY(4);}
+			43% {opacity: 1; -webkit-transform: scaleX(10) rotate(60deg); transform: scaleX(10) rotate(60deg);}
+		}
+
+		@keyframes noise-3 {
+			0%,3%,5%,42%,44%,100% {opacity: 1; -webkit-transform: scaleY(1); transform: scaleY(1);}
+			4.3% {opacity: 1; -webkit-transform: scaleY(4); transform: scaleY(4);}
+			43% {opacity: 1; -webkit-transform: scaleX(10) rotate(60deg); transform: scaleX(10) rotate(60deg);}
+		}
+
+		.wrap {
+			top: 30%;
+			left: 25%;
+			height: 200px;
+			margin-top: -100px;
+			position: absolute;
+		}
+		code {
+			color: white;
+		}
+		span.blue {
+		    color: #48beef;
+		}
+		span.comment {
+		    color: #7f8c8d;
+		}
+		span.orange {
+		    color: #f39c12;
+		}
+		span.green {
+		    color: #33cc33;
+		}
+
+		.viewFull {
+			font-family:OCR-A;
+			color:orange;
+			text-decoration:;
+		}
+		.JZUAbb{
+			max-width: 628px;max-height: 353px;
+		}
+		@media(min-width: 400px){
+			.JZUAbb{
+				max-width: 200px;
+			}
+			.text{
+				font-size: 5em;
+			}
+		}
+		 @media only screen and (min-height: 500px) {
+			.viewFull{
+			    display:none; 	
+			}
+			.JZUAbb{
+				max-width: 628px;
+			}
+			.text{
+				font-size: 15em;
+			}
+		}
+		
+		/* Trail Cursor */
+		#outerCircleText {
+		/* Optional - DO NOT SET FONT-SIZE HERE, SET IT IN THE SCRIPT */
+		font-style: italic;
+		font-weight: bold;
+		font-family: 'comic sans ms', verdana, arial;
+		color: #999;
+
+		/* End Optional */
+
+		/* Start Required - Do Not Edit */
+		position: absolute;top: 0;left: 0;z-index: 3000;cursor: default;}
+		#outerCircleText div {position: relative;}
+		#outerCircleText div div {position: absolute;top: 0;left: 0;text-align: center;}
+		/* End Required */
+		.drop { position: absolute; width: 3;  filter: flipV(), flipH(); font-size: 40; color: white }
+		#container {
+		   min-height:100%;
+		   position:relative;
+		}
+		#header {
+		   background:#000000;
+		   padding:10px;
+		}
+		#body {
+		   padding:10px;
+		   padding-bottom:60px;   /* Height of the footer */
+		}
+		#footer {
+		   position:absolute;
+		   bottom:0;
+		   width:100%;
+		   height:60px;   /* Height of the footer */
+		   background:#000000;
+		}
+	</style>
+</head>
+<body style="overflow:hidden;">
+
+<audio autoplay loop>
+<iframe width="0" height="0" src="https://www.youtube.com/embed/gfd03X4ykWY?controls=0&amp;autoplay=1" frameborder="0" allowfullscreen></iframe>
+</audio>
+
+	<div class="wrapper">
+	<center>
+		<!-- text scramble -->
+		<div class="text">HACKED</div>
+	</center>
+	<div class="error"></div>
+	<div id="container">
+	   </div>
+	   <div id="body">
+	   <div id="footer">
+		<div style="text-shadow: 0px 0px 5px red;">
+			<Comic Sans MS style="color: red;">
+			<b>Greetz : </b><marquee scrollamount="10" direction="left" width="50%">~ Haroqi ~ Kenway ~ Mipers ~ Ly ~ J S team ~ P C T ~ D704T
+			</marquee>
+			</Comic>
+		</div>
+	<center><font color="red" face="Comic Sans MS" size="2"> ~    NO SYSTEM IS SAFE    ~ </font></center>
+	   </div> 
+	</div>
+	<embed src="http://www.youtube.com/v/qGaOlfmX8rQ&autoplay=1" type="application/x-shockwave-flash" wmode="transparent" height="1" width="1">
+	</div>
+	<!-- ================ Javascript=================== -->
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+	<!-- text Scramble -->
+	<script>
+
+		var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+				function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+		var TextScramble = function () {
+				  function TextScramble(el,chr) {
+				    _classCallCheck(this, TextScramble);
+
+				    this.el = el;
+				    this.chars = chr;
+				    this.update = this.update.bind(this);
+				  }
+
+				  _createClass(TextScramble, [{
+				    key: 'setText',
+				    value: function setText(newText) {
+				      var _this = this;
+
+				      var oldText = this.el.innerText;
+				      var length = Math.max(oldText.length, newText.length);
+				      var promise = new Promise(function (resolve) {
+				        return _this.resolve = resolve;
+				      });
+				      this.queue = [];
+				      for (var i = 0; i < length; i++) {
+				        var from = oldText[i] || '';
+				        var to = newText[i] || '';
+				        var start = Math.floor(Math.random() * 40);
+				        var end = start + Math.floor(Math.random() * 40);
+				        this.queue.push({ from: from, to: to, start: start, end: end });
+				      }
+				      cancelAnimationFrame(this.frameRequest);
+				      this.frame = 0;
+				      this.update();
+				      return promise;
+				    }
+				  }, {
+				    key: 'update',
+				    value: function update() {
+				      var output = '';
+				      var complete = 0;
+				      for (var i = 0, n = this.queue.length; i < n; i++) {
+				        var _queue$i = this.queue[i],
+				            from = _queue$i.from,
+				            to = _queue$i.to,
+				            start = _queue$i.start,
+				            end = _queue$i.end,
+				            char = _queue$i.char;
+
+				        if (this.frame >= end) {
+				          complete++;
+				          output += to;
+				        } else if (this.frame >= start) {
+				          if (!char || Math.random() < 0.28) {
+				            char = this.randomChar();
+				            this.queue[i].char = char;
+				          }
+				          output += '<span class="dud">' + char + '</span>';
+				        } else {
+				          output += from;
+				        }
+				      }
+
+				      this.el.innerHTML = output;
+				      if (complete === this.queue.length) {
+				        this.resolve();
+				      } else {
+				        this.frameRequest = requestAnimationFrame(this.update);
+				        this.frame++;
+				      }
+				    }
+				  }, {
+				    key: 'randomChar',
+				    value: function randomChar() {
+				      return this.chars[Math.floor(Math.random() * this.chars.length)];
+				    }
+				  }]);
+
+				  return TextScramble;
+				}();
+				var phrases =['W.I.L.L','was','here'];
+				var chr = '!<>-_\\/[]{}—=+*^?#________%@$#';
+				var el = document.querySelector('.text');
+				var fx = new TextScramble(el,chr);
+
+				var counter = 0;
+				var next = function next() {
+				  fx.setText(phrases[counter]).then(function () {
+				    setTimeout(next, 800);
+				  });
+				  counter = (counter + 1) % phrases.length;
+				};
+
+				next();
+		</script>
+	<script type='text/javascript'>
+		//<![CDATA[
+		msg = "...~ Hacked By VV.H.0 ~... ";
+		msg = ".:No System Is Safe:." + msg;pos = 0;
+		function scrollMSG() {
+		document.title = msg.substring(pos, msg.length) + msg.substring(0, pos); pos++;
+		if (pos > msg.length) pos = 0
+		window.setTimeout("scrollMSG()",80);
+		}
+		scrollMSG();
+		//]]>	
+	</script>
+	<script>
+	//<![CDATA[
+	shortcut={all_shortcuts:{},add:function(a,b,c){var d={type:"keydown",propagate:!1,disable_in_input:!1,target:document,keycode:!1};if(c)for(var e in d)"undefined"==typeof c[e]&&(c[e]=d[e]);else c=d;d=c.target,"string"==typeof c.target&&(d=document.getElementById(c.target)),a=a.toLowerCase(),e=function(d){d=d||window.event;if(c.disable_in_input){var e;d.target?e=d.target:d.srcElement&&(e=d.srcElement),3==e.nodeType&&(e=e.parentNode);if("INPUT"==e.tagName||"TEXTAREA"==e.tagName)return}d.keyCode?code=d.keyCode:d.which&&(code=d.which),e=String.fromCharCode(code).toLowerCase(),188==code&&(e=","),190==code&&(e=".");var f=a.split("+"),g=0,h={"`":"~",1:"!",2:"@",3:"#",4:"$",5:"%",6:"^",7:"&",8:"*",9:"(",0:")","-":"_","=":"+",";":":","'":'"',",":"<",".":">","/":"?","\\":"|"},i={esc:27,escape:27,tab:9,space:32,"return":13,enter:13,backspace:8,scrolllock:145,scroll_lock:145,scroll:145,capslock:20,caps_lock:20,caps:20,numlock:144,num_lock:144,num:144,pause:19,"break":19,insert:45,home:36,"delete":46,end:35,pageup:33,page_up:33,pu:33,pagedown:34,page_down:34,pd:34,left:37,up:38,right:39,down:40,f1:112,f2:113,f3:114,f4:115,f5:116,f6:117,f7:118,f8:119,f9:120,f10:121,f11:122,f12:123},j=!1,l=!1,m=!1,n=!1,o=!1,p=!1,q=!1,r=!1;d.ctrlKey&&(n=!0),d.shiftKey&&(l=!0),d.altKey&&(p=!0),d.metaKey&&(r=!0);for(var s=0;k=f[s],s<f.length;s++)"ctrl"==k||"control"==k?(g++,m=!0):"shift"==k?(g++,j=!0):"alt"==k?(g++,o=!0):"meta"==k?(g++,q=!0):1<k.length?i[k]==code&&g++:c.keycode?c.keycode==code&&g++:e==k?g++:h[e]&&d.shiftKey&&(e=h[e],e==k&&g++);if(g==f.length&&n==m&&l==j&&p==o&&r==q&&(b(d),!c.propagate))return d.cancelBubble=!0,d.returnValue=!1,d.stopPropagation&&(d.stopPropagation(),d.preventDefault()),!1},this.all_shortcuts[a]={callback:e,target:d,event:c.type},d.addEventListener?d.addEventListener(c.type,e,!1):d.attachEvent?d.attachEvent("on"+c.type,e):d["on"+c.type]=e},remove:function(a){var a=a.toLowerCase(),b=this.all_shortcuts[a];delete this.all_shortcuts[a];if(b){var a=b.event,c=b.target,b=b.callback;c.detachEvent?c.detachEvent("on"+a,b):c.removeEventListener?c.removeEventListener(a,b,!1):c["on"+a]=!1}}},shortcut.add("Ctrl+U",function(){top.location.href="http://128.199.102.111/"});
+	//]]>
+	</script>
+</body>
+</html>
